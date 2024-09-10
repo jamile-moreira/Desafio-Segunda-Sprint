f76fb3462a22e5564224035f854187c158e04184
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-09-09 22:44:51 -0300

    Inicio manipulação dom na página login

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function login(event) {
+  event.preventDefault()
+
+  const user = document.getElementById('exampleInputEmail1').value
+  const password = document.getElementById('exampleInputPassword1').value
+}
