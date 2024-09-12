190833dd0c30af225bdf219a252943d53985dea2
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-09-12 10:07:53 -0300

    pagina home nav

--- assets/pages/Home/home.js
+++ assets/pages/Home/home.js
@@ -1,1 +1,11 @@
-import './style.css'
+function sair(event) {
+  event.preventDefault()
+
+  const deslogar = document.getElementById('btnSair')
+
+  if (deslogar) {
+    window.location.href = '../../../index.html'
+  }
+}
+
+document.getElementById('btnSair').addEventListener('click', sair)
